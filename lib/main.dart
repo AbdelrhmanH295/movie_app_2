86eb3e0e8641c21b4856eb3cdb64b62652f561dc
Abdelrhman Hammam <abodyhard295@gmail.com>
2025-09-05 16:17:11 +0300

    Auth folder created

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/screens/explore_tab.dart';
-import 'package:movie_app/screens/forget_password.dart';
+import 'package:movie_app/screens/Auth/forget_password.dart';
 import 'package:movie_app/screens/home_screen.dart';
 import 'package:movie_app/screens/home_tab.dart';
-import 'package:movie_app/screens/login_screen.dart';
+import 'package:movie_app/screens/Auth/login_screen.dart';
 import 'package:movie_app/screens/on_boarding.dart';
 import 'package:movie_app/screens/profile_update_tab.dart';
-import 'package:movie_app/screens/register_screen.dart';
-import 'package:movie_app/screens/reset_password_screen.dart';
+import 'package:movie_app/screens/Auth/register_screen.dart';
+import 'package:movie_app/screens/Auth/reset_password_screen.dart';
 import 'package:movie_app/screens/search_tab.dart';
 import 'package:movie_app/utils/app_routes.dart';
 
